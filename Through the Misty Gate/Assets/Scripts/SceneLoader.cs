183c258891d51df8223826619fdec46f5f7633e3
Paulina <paulinajuzwin@gmail.com>
2022-06-02 00:12:54 +0100

    Add scene loader.

---
+++ Through the Misty Gate/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader instance;
+
+    public float loadTime = 5.0f;
+    
+    public enum SceneName
+    {
+        TitleScreenScene,
+        LoadingScene,
+        GameScene    
+    }
+    
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+
+    public void LoadScene(SceneName name)
+    {
+        StartCoroutine(SceneLoading(name));
+    }
+
+    private IEnumerator SceneLoading(SceneName name)
+    {
+        SceneManager.LoadScene(SceneName.LoadingScene.ToString());
+
+        yield return new WaitForSeconds(loadTime);
+        
+        SceneManager.LoadScene(name.ToString());
+    }
+}
+
+
