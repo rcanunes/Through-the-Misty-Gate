bc810aa7204c56a1cef9017e97b02d6b4388fc06
MrIceHavoc <afonso.fernandes100@gmail.com>
2022-06-04 15:59:55 +0100

    Health

---
+++ Through the Misty Gate/Assets/Scripts/Health.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour
+{
+    public float maxHealth = 100.0f;
+    public float curHealth = 0.0f;
+    public float burstRate = 1.5f;
+    private float nextBurst = 0.0f;
+    private float nextDamage = 0.0f;
+    public float damageRate = 2.0f;
+    public Image m1Image;
+    public Image m2Image;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        curHealth = maxHealth;
+        m1Image = GameObject.FindGameObjectWithTag("M1").GetComponent<Image>();
+        m2Image = GameObject.FindGameObjectWithTag("M2").GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddHealth(int hp) {
+        curHealth += hp;
+    }
+}
