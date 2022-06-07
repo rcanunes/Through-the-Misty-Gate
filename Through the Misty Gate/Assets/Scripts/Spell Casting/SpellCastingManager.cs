6cb01ec2683d8d18bbf74f544f1196579a733c9f
Paulina <paulinajuzwin@gmail.com>
2022-06-07 20:13:52 +0100

    Add spell database scriptable object.

---
+++ Through the Misty Gate/Assets/Scripts/Spell Casting/SpellCastingManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellCastingManager : MonoBehaviour
+{
+    private List<SpellScriptableObject> _unlockedSpells;
+
+    [SerializeField] private SpellsDatabase spellsDatabase;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
