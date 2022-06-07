88df2b0cbbaefdd1fc847583d7e13f7463cff35f
rcanunes <rodrigo.ca.nunes99@gmail.com>
2022-06-07 09:59:43 +0100

    Individual enemy files

---
+++ Through the Misty Gate/Assets/Scripts/Enemies/Enemies/Ruins/MageRuins.cs
@@ -0,0 +1,11 @@
+﻿using Enemies.EnemyTypes;
+
+namespace Enemies.Enemies.Ruins {
+    public class MageRuins : MageEnemy {
+        protected override void Start() {
+            base.Start();
+
+            //Set attributes
+        }
+    }
+}
---
+++ Through the Misty Gate/Assets/Scripts/Enemies/Enemies/Ruins/TurretRuins.cs
@@ -0,0 +1,11 @@
+﻿using Enemies.EnemyTypes;
+
+namespace Enemies.Enemies.Ruins {
+    public class TurretRuins : TurretEnemy {
+        protected override void Start() {
+            base.Start();
+
+            //Set attributes
+        }
+    }
+}
