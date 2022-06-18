e12d2112b24e5b80a2f8b96526cf911d67790036
rcanunes <rodrigo.ca.nunes99@gmail.com>
2022-06-19 00:19:11 +0100

    Fix namespaces

--- Through the Misty Gate/Assets/Scripts/Spell Casting/SpellEffectsScriptableObject.cs
+++ Through the Misty Gate/Assets/Scripts/Spell Casting/SpellEffectsScriptableObject.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using Player;
 
-public abstract class SpellEffectsScriptableObject : ScriptableObject
-{
-  // Parent class for specialized spells
-  public abstract void Cast(MonoBehaviour caller, PlayerController player);
+namespace Spell_Casting {
+  public abstract class SpellEffectsScriptableObject : ScriptableObject {
+    // Parent class for specialized spells
+    public abstract void Cast(MonoBehaviour caller, PlayerController player);
+  }
 }
--- Through the Misty Gate/Assets/Scripts/Spells UI/SpellBook/SpellBookSystem.cs
+++ Through the Misty Gate/Assets/Scripts/Spells UI/SpellBook/SpellBookSystem.cs
@@ -2,41 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Spell_Casting;
 
-public class SpellBookSystem
-{
-    private SpellCastingManager player;
-    private List<UI_ItemManager.HotKeyAbility> allSpells;
-    public event EventHandler OnSpellChange;
+namespace Spells_UI.Spellbook {
+    public class SpellBookSystem {
+        private SpellCastingManager player;
+        private List<UI_ItemManager.HotKeyAbility> allSpells;
+        public event EventHandler OnSpellChange;
 
-    public bool toogleSpellBook;
+        public bool toogleSpellBook;
 
-    public SpellBookSystem(SpellCastingManager player)
-    {
-        this.player = player;
-        //Adding to Inventory of Spells
+        public SpellBookSystem(SpellCastingManager player) {
+            this.player = player;
+            //Adding to Inventory of Spells
 
-        allSpells = new List<UI_ItemManager.HotKeyAbility>();
+            allSpells = new List<UI_ItemManager.HotKeyAbility>();
 
-        foreach (var s in player.GetUnlockedSpells())
-        {
-            allSpells.Add(new UI_ItemManager.HotKeyAbility
-            {
-                spell = s,
-                spellId = s.spellId,
-                activateSpell = () => player.SetCurrentSpell(s.spellId)
-            });
+            foreach (var s in player.GetUnlockedSpells()) {
+                allSpells.Add(new UI_ItemManager.HotKeyAbility {
+                    spell = s,
+                    spellId = s.spellId,
+                    activateSpell = () => player.SetCurrentSpell(s.spellId)
+                });
+            }
         }
-    }
 
-    public List<UI_ItemManager.HotKeyAbility> GetAllSpells()
-    {
-        return allSpells;
-    }
+        public List<UI_ItemManager.HotKeyAbility> GetAllSpells() {
+            return allSpells;
+        }
 
-    public void InvokeOnSpellChange()
-    {
-        OnSpellChange?.Invoke(this, EventArgs.Empty);
-    }
+        public void InvokeOnSpellChange() {
+            OnSpellChange?.Invoke(this, EventArgs.Empty);
+        }
 
+    }
 }
